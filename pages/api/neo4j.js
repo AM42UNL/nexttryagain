1f03747f664e7150a82bcc6d632b9d60dd0ac2f9
AM42UNL <104471408+AM42UNL@users.noreply.github.com>
2022-05-04 18:32:36 -0500

    Changed Query v2

--- pages/api/neo4j.js
+++ pages/api/neo4j.js
@@ -8,8 +8,8 @@ const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
 
 export default async function handler(req, res) {
     const session = driver.session();
-    const response = await session.run('MATCH (quote:Quote {complexity:"Low"})-[:fromWork] -> (work:Work) - [typeOf] ->(type:Type) RETURN quote, work, type');
-
+    //const response = await session.run('MATCH (quote:Quote {complexity:"Low"})-[:fromWork] -> (work:Work) - [typeOf] ->(type:Type) RETURN quote, work, type');
+    const response = await session.run('MATCH (quote:Quote {complexity:"Low"})-[:fromWork] -> (work:Work) - [typeOf] ->(type:Type) RETURN quote');
     const records =  response.records.map((record) => {
         return record.toObject();
     })
