4acc059459d296294d5567700a2159b405ae76cc
AM42UNL <104471408+AM42UNL@users.noreply.github.com>
2022-05-05 16:47:36 -0500

    Changed next.config.js v2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-// const nextConfig = {
+//const nextConfig = {
 
 // reactStrictMode: true,
 
@@ -24,13 +24,16 @@ module.exports = {
 
         headers: [
 
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {key: "Access-Control-Allow-Credentials", value: "true"},
 
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {key: "Access-Control-Allow-Origin", value: "*"},
 
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          {key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"},
 
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+          },
 
         ]
 
@@ -39,3 +42,4 @@ module.exports = {
     ]
 
   }
+}
