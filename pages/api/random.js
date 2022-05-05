cb293bff7c8459d8372e2375e1d1b4c3cfd4451c
AM42UNL <104471408+AM42UNL@users.noreply.github.com>
2022-05-05 17:50:42 -0500

    Added Random

---
+++ pages/api/random.js
@@ -0,0 +1,19 @@
+import neo4j from "neo4j-driver";
+
+
+let dataResponse;
+const uri = 'neo4j+s://0436e85b.databases.neo4j.io';
+const user = 'neo4j';
+const password = process.env.NEO4J;
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+
+export default async function handler(req, res) {
+    const session = driver.session();
+    //const response = await session.run('MATCH (quote:Quote {complexity:"Low"})-[:fromWork] -> (work:Work) - [typeOf] ->(type:Type) RETURN quote, work, type');
+    const response = await session.run('MATCH (quote:Quote {complexity:"Low"})-[:fromWork] -> (work:Work) - [typeOf] ->(type:Type) RETURN quote, work ORDER BY rand() LIMIT 1');
+    const records =  response.records.map((record) => {
+        return record.toObject();
+    })
+    res.status(200).json({body: records});
+}
